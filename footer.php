65528c5dd5330c0489e4279a11765192f14c3a27
Alex Dodkin <alex@eggplantdigital.cn>
2015-04-16 05:38:11 +0800

    Move footer actions to footer.php file

--- footer.php
+++ footer.php
@@ -9,29 +9,70 @@
  * @since Salt 1.0
  */
 
-do_action('salt_footer_above'); ?>
-
-	<footer id="footer" <?php do_action('salt_footer_class'); ?>>
-
-		<?php
-		do_action('salt_footer_inside_above');
-
-		do_action('salt_footer_inside');
-
-		do_action('salt_footer_inside_below');
-		?>
-
-	</footer>
+ 	do_action('salt_footer_above'); ?>
 
 	<?php
-	do_action('salt_footer_below');
+	$cols = false;
+	$cols = get_theme_mod('salt_footer_sidebars');
+
+	if ($cols != 0) {
 	
-	do_action('salt_bottom');
-	
-	wp_footer();
+	if ($cols==6)
+		$span = 'col-sm-2';
+	elseif ($cols==4)
+		$span = 'col-sm-3';
+	elseif ($cols==3)
+		$span = 'col-sm-4';
+	elseif ($cols==2)
+		$span = 'col-sm-6';
+	elseif ($cols==1)
+		$span = 'col-sm-12';
+		
+	$i = 0;
 	?>
+	<div id="footer-widgets">
+		<div class="container">
+			<div class="row">
+			<?php
+			while ( $i < $cols ) {
+				$i++;
+				?>
+				<div class="<?php echo $span; ?>">	
+					<?php
+					$footer_siderbar = 'footer-' . $i;
+					dynamic_sidebar( $footer_siderbar ); ?>
+				</div>
+			<?php } ?>
+			</div>
+		</div>
+	</div>	
+	<?php } ?>
+
+	<div id="footer-wrapper">		
+		<footer id="footer" <?php do_action('salt_footer_class'); ?>>
+	
+			<?php do_action('salt_footer_inside_above'); ?>
+
+			<p class="pull-left copyright"><span class="copy">&copy;</span> <?php echo date('Y'); ?> <?php echo get_theme_mod('salt_footer_text'); ?></p>
+	
+		    <?php if (get_option('salt_footer_credit')!='1') { ?>
+			<p class="pull-right credits">
+				<?php echo sprintf(__('Designed by %1s charity: themes %2s', 'salt'), '<a class="ct_logo" href="http://www.charitythemes.org" title="WordPress Themes for Nonprofits" target="_blank">','</a>'); ?>
+			</p>
+			<?php } ?>
+	
+			<?php do_action('salt_footer_inside_below'); ?>
+	
+		</footer>
+	</div>
+
+	<?php do_action('salt_footer_below'); ?>
+	
+	<?php do_action('salt_bottom'); ?>
 	
 	</div><!-- /#wrapper -->
+
+<?php wp_footer(); ?>
 
 </body>
 
